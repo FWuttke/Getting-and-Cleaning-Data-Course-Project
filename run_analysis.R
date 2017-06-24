dd5d8b35585a013c51c3c6a3b3c9faac72089054
FWuttke <fernando.wuttke@gmail.com>
2017-06-24 13:31:34 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,38 +2,38 @@ library(data.table)
 library(reshape2)
 
 #Load the dimension´s labels
-y_labels <- read.table("R_Datasets/UCI_HAR_Dataset/activity_labels.txt")
-x_labels <- read.table("R_Datasets/UCI_HAR_Dataset/features.txt")
+y_labels <- read.table("./UCI_HAR_Dataset/activity_labels.txt")
+x_labels <- read.table("./UCI_HAR_Dataset/features.txt")
 
 #Load the test measures data and keep just the required columns
-x_test <- read.table("R_Datasets/UCI_HAR_Dataset/test/X_test.txt")
+x_test <- read.table("./UCI_HAR_Dataset/test/X_test.txt")
 names(x_test) = x_labels[,2]
 x_required <- grepl("mean|std", x_labels[,2])
 x_test = x_test[,x_required]
 
 #Load the test activity data and change the Activity ID by Activity name on the data table
-y_test <- read.table("R_Datasets/UCI_HAR_Dataset/test/Y_test.txt")
+y_test <- read.table("./UCI_HAR_Dataset/test/Y_test.txt")
 y_test <- merge(y_test,y_labels,by.x = "V1",by.y = "V1",all = TRUE)
 y_test <- as.data.table(y_test[,2])
 names(y_test) = "Activity"
 
 #Load the test subject data
-s_test <- read.table("R_Datasets/UCI_HAR_Dataset/test/subject_test.txt", col.names = 'Subject')
+s_test <- read.table("./UCI_HAR_Dataset/test/subject_test.txt", col.names = 'Subject')
 
 #Append all the test data into one table
 dt_test <- cbind(s_test, y_test, x_test)
 
 #Repeat the same process above to the train data
-x_train <- read.table("R_Datasets/UCI_HAR_Dataset/train/X_train.txt")
+x_train <- read.table("./UCI_HAR_Dataset/train/X_train.txt")
 names(x_train) = x_labels[,2]
 x_train = x_train[,x_required]
 
-y_train <- read.table("R_Datasets/UCI_HAR_Dataset/train/Y_train.txt")
+y_train <- read.table("./UCI_HAR_Dataset/train/Y_train.txt")
 y_train <- merge(y_train,y_labels,by.x = "V1",by.y = "V1",all = TRUE)
 y_train <- as.data.table(y_train[,2])
 names(y_train) = "Activity"
 
-s_train <- read.table("R_Datasets/UCI_HAR_Dataset/train/subject_train.txt", col.names = 'Subject')
+s_train <- read.table("./UCI_HAR_Dataset/train/subject_train.txt", col.names = 'Subject')
 
 dt_train <- cbind(s_train, y_train, x_train)
 
